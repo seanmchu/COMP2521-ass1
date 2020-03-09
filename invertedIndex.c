bbfa581a01e7d05f586d6352ae9a9dd4df321c74
seanmchu <seanmorotachu@gmail.com>
2020-03-09 11:48:12 +1100

    added example files

--- invertedIndex.c
+++ invertedIndex.c
@@ -1,1 +1,5 @@
+//Comp2521 assignment 1: information retreival
+//Written by Sean Morota Chu (z5259673), started 09/03/2020 - completed xx/xx/2020
+//A function to read files listed in collection.txt and implement functions in invertedIndex.h for full functionality.
 #include"invertedIndex.h"
+
