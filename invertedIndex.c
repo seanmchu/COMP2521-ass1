c842549f6e1aeeff67eb83f7e1a557b733c22bd1
seanmchu <seanmorotachu@gmail.com>
2020-03-09 11:57:21 +1100

    Added a line

--- invertedIndex.c
+++ invertedIndex.c
@@ -2,4 +2,24 @@
 //Written by Sean Morota Chu (z5259673), started 09/03/2020 - completed xx/xx/2020
 //A function to read files listed in collection.txt and implement functions in invertedIndex.h for full functionality.
 #include"invertedIndex.h"
+char *normaliseWord(char *str){
 
+    return str;
+}
+
+
+InvertedIndexBST generateInvertedIndex(char *collectionFilename){
+
+    return 0;
+}
+
+void printInvertedIndex(InvertedIndexBST tree) {
+
+    return;
+}
+TfIdfList calculateTfIdf(InvertedIndexBST tree, char *searchWord, int D) {
+    return;
+}
+TfIdfList retrieve(InvertedIndexBST tree, char *searchWords[], int D) {
+    return;
+}
